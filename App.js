e13ee494fcae53884a38e2e8dd11a2ceeff0864a
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-04-21 18:48:24 -0700

    react native demo app with react-native-video

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Video from 'react-native-video';
+
+const App: () => React$Node = () => {
+  const [showVideo, setShowVideo] = useState(false);
+
+  const onSeek = () => {
+    // this only gets logged at the end of seek events on Android. It does not get logged on iOS
+    console.log('debug onSeek');
+  };
+
+  const onProgress = () => {
+    console.log('debug onProgress');
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <Button
+              title={showVideo ? 'Hide' : 'Show'}
+              onPress={() => setShowVideo(!showVideo)}
+            />
+            {showVideo && (
+              <Video
+                style={styles.video}
+                source={{
+                  uri:
+                    'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+                }}
+                onSeek={onSeek}
+                onProgress={onProgress}
+                controls
+                muted
+              />
+            )}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  video: {
+    width: 400,
+    height: 500,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
